f4deb7a2aa15274fb76d472caaa9b02dbd7d7b30
Kushagra Saxena <128407478+kushagra1934@users.noreply.github.com>
2024-11-14 16:15:53 +0530

    Create binary_exponentiation.cpp
    program for binary exponentiation O(logn)

---
+++ binary_exponentiation.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+long long binpow(long long a, long long b) {
+    if (b == 0)
+        return 1;
+    long long res = binpow(a, b / 2);
+    if (b % 2)
+        return res * res * a;
+    else
+        return res * res;
+}
+
+
+int main(){
+    int a,b;
+    cin>>a>>b;
+    cout<<binpow(a,b)<<endl;
+}
